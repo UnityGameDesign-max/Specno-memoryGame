8b6d1860f8378529d85225582cb2c6e62424fe15
Tumelo <tumie201511@gmail.com>
2023-02-21 03:10:21 +0200

    updated card images as constants

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,3 +1,194 @@
-const Colors = {
-    White: '#FFFFFF',   
+import { 
+    Environment 
+} from "common/environment";
+
+export const Colors = {
+    White: '#FFFFFF',
+    Green: '#0AB169',
+    Red: '#D4190C',
+    Orange: '#F4A236',
+    Grey: 'lightGrey'
+};
+export const FontSizes = {
+    Tiny: '1.2vmin',
+    Smallest:'1.75vmin',
+    Smaller:'2.15vmin',
+    Small:'2.5vmin',
+    Normal:'3.15vmin',
+    Big:'3.5vmin',
+    Bigger:'4vmin',
+    Biggest:'5.1vmin',
+    Huge:'6.5vmin'
+};
+
+export const Icons = {
+    Player: `${Environment.assetBase}/icons/player1.svg`,
+    Opponent: `${Environment.assetBase}/icons/player2.svg`,
+};
+
+export const Cards = [
+    {
+        src: `${Environment.assetBase}/cards/clubs/2.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/clubs/3.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/clubs/4.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/clubs/5.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/clubs/6.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/clubs/7.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/clubs/8.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/clubs/9.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/clubs/10.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/clubs/A.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/clubs/J.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/clubs/K.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/clubs/Q.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/diamonds/2.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/diamonds/3.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/diamonds/4.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/diamonds/5.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/diamonds/6.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/diamonds/7.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/diamonds/8.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/diamonds/9.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/diamonds/10.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/diamonds/A.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/diamonds/J.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/diamonds/K.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/hearts/2.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/hearts/3.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/hearts/4.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/hearts/2.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/hearts/3.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/hearts/4.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/hearts/5.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/hearts/6.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/hearts/7.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/hearts/8.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/hearts/9.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/hearts/10.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/hearts/A.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/hearts/J.svg`,
+        matched: false
+    },
+    {
+        src: `${Environment.assetBase}/cards/hearts/K.svg`,
+        matched: false
+    },
+];
+
+export const Cover = {
+    src: `${Environment.assetBase}/icons/Cover.png`
 }
