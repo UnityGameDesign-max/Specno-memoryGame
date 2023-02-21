07724800f10b65d8c4cf040a02185d66eeb3def8
Tumelo <tumie201511@gmail.com>
2023-02-21 03:14:19 +0200

    updated global styles for different button classes

--- src/common/styles.js
+++ src/common/styles.js
@@ -1,3 +1,41 @@
-const Colors = {
-    White: '#FFFFFF',   
-}
+import 
+    styled,
+    { css }
+from 'styled-components';
+
+import { 
+    Colors,
+    FontSizes 
+} from 'common/constants';
+
+const BaseButtonStyles = css`
+    color: ${Colors.White};
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+`;
+
+export const HorizontalSpace = styled.div`
+    width: '0.5vw';
+`;
+export const PrimaryButton = styled.button`
+    ${BaseButtonStyles};
+    background-color: ${Colors.Green};
+    padding: 1vh 3vw;
+    font-size: ${FontSizes.Small};
+`;
+
+export const ExitButton = styled.button`
+    ${BaseButtonStyles};
+    background-color: ${Colors.Red};
+    padding: 1vh 2vw;
+    font-size: ${FontSizes.Small};
+`;
+
+export const RestartButton = styled.button`
+    ${BaseButtonStyles};
+    background-color: ${Colors.Orange};
+    padding: 1vh 0.3vw;
+    font-size: ${FontSizes.Smaller};
+    margin-left: auto;
+`;
